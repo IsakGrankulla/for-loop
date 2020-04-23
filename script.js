10c2a73319a5004c8f9e17c5013492190ad0e29f
IsakGrankulla <isakgrankulla@gmail.com>
2020-04-23 12:10:24 +0300

    update

---
+++ script.js
@@ -0,0 +1,17 @@
+let input2 = document.getElementById('numberinput')
+
+let number = parseInt(numberinput.value);
+let form = document.getElementById('form');
+let p = document.getElementById('text-content');
+
+form.addEventListener('submit', onButtonClicked);
+
+function onButtonClicked(event) {
+    let number = parseInt(numberinput.value);
+    event.preventDefault();
+
+for (let i = 0; i < numberinput.value; i++) {
+    p.innerHTML += "Test" + "<br>";
+    
+}
+}
